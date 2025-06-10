1df05a270316a31798bce020750e081c47d60093
Nyckjeni <nycole.jenifer366@gmail.com>
2025-06-10 09:09:50 -0300

    exclusão backend

--- backend/routes/vitimaRoutes.js
+++
@@ -1,9 +0,0 @@
-// vitimaRoutes.js
-const express = require('express');
-const router = express.Router();
-const vitimaController = require('../controllers/vitimaController');
-
-router.post('/', vitimaController.createVitima);
-router.get('/', vitimaController.listVitimas);
-
-module.exports = router;
